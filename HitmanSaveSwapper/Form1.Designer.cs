a4bb5fb6c29a955202401d80a9fa5d10337395e0
Jackz <me@jackz.me>
2018-10-11 07:16:31 -0500

    Add project files.

---
+++ HitmanSaveSwapper/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace HitmanSaveSwapper {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.saveProfile1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveProfile3 = new System.Windows.Forms.RadioButton();
+            this.saveProfile4 = new System.Windows.Forms.RadioButton();
+            this.saveProfile2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // saveProfile1
+            // 
+            this.saveProfile1.AutoSize = true;
+            this.saveProfile1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveProfile1.Location = new System.Drawing.Point(19, 37);
+            this.saveProfile1.Name = "saveProfile1";
+            this.saveProfile1.Size = new System.Drawing.Size(14, 13);
+            this.saveProfile1.TabIndex = 0;
+            this.saveProfile1.TabStop = true;
+            this.saveProfile1.Tag = "1";
+            this.saveProfile1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.saveProfile3);
+            this.groupBox1.Controls.Add(this.saveProfile4);
+            this.groupBox1.Controls.Add(this.saveProfile2);
+            this.groupBox1.Controls.Add(this.saveProfile1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(3, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(354, 223);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(39, 146);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(181, 31);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(39, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 31);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "fag";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(39, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 31);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Eli";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 31);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Heavenira";
+            // 
+            // saveProfile3
+            // 
+            this.saveProfile3.AutoSize = true;
+            this.saveProfile3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveProfile3.Location = new System.Drawing.Point(19, 115);
+            this.saveProfile3.Name = "saveProfile3";
+            this.saveProfile3.Size = new System.Drawing.Size(14, 13);
+            this.saveProfile3.TabIndex = 3;
+            this.saveProfile3.TabStop = true;
+            this.saveProfile3.Tag = "3";
+            this.saveProfile3.UseVisualStyleBackColor = true;
+            // 
+            // saveProfile4
+            // 
+            this.saveProfile4.AutoSize = true;
+            this.saveProfile4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveProfile4.Location = new System.Drawing.Point(19, 155);
+            this.saveProfile4.Name = "saveProfile4";
+            this.saveProfile4.Size = new System.Drawing.Size(14, 13);
+            this.saveProfile4.TabIndex = 2;
+            this.saveProfile4.TabStop = true;
+            this.saveProfile4.Tag = "4";
+            this.saveProfile4.UseVisualStyleBackColor = true;
+            // 
+            // saveProfile2
+            // 
+            this.saveProfile2.AutoSize = true;
+            this.saveProfile2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveProfile2.Location = new System.Drawing.Point(19, 74);
+            this.saveProfile2.Name = "saveProfile2";
+            this.saveProfile2.Size = new System.Drawing.Size(14, 13);
+            this.saveProfile2.TabIndex = 1;
+            this.saveProfile2.TabStop = true;
+            this.saveProfile2.Tag = "2";
+            this.saveProfile2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(369, 227);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "HitmanSaveSwapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton saveProfile1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton saveProfile3;
+        private System.Windows.Forms.RadioButton saveProfile4;
+        private System.Windows.Forms.RadioButton saveProfile2;
+    }
+}
+
---
+++ HitmanSaveSwapper/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HitmanSaveSwapper {
+    public partial class Form1 : Form {
+        static string APPDATA_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); // AppData folder
+        static string SAVEPATH = Path.Combine(APPDATA_PATH, "HitmanSaveSwapper");
+        private static DirectoryInfo HITMANPATH = new DirectoryInfo(@"D:\Jackz\Documents\_temp\hitmanshit\remote");
+        public Form1() {
+            InitializeComponent();
+            if (Properties.Settings.Default.profilenum <= 0) Properties.Settings.Default.profilenum = 1;
+
+            saveProfile1.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            saveProfile2.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            saveProfile3.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            saveProfile4.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+
+            textBox1.TextChanged += new EventHandler(textBox_Changed);
+            textBox2.TextChanged += new EventHandler(textBox_Changed);
+            textBox3.TextChanged += new EventHandler(textBox_Changed);
+            textBox4.TextChanged += new EventHandler(textBox_Changed);
+        }
+
+        /* TODO:
+         * On (EventHandler) textbox change -> save
+         * On Load -> save
+         * Finish hitman -> save
+         * Copy from save -> hitman 
+         */
+
+        private void Form1_Load(object sender, EventArgs e) {
+            if (!Properties.Settings.Default.setup)
+            {
+                DialogResult result = MessageBox.Show("Do you want to copy your save data to %appdata%\\HitmanSaveSwapper?",
+                    "Set up Swapper?", MessageBoxButtons.YesNo);
+                if (DialogResult.Yes == result) {
+
+                } else {
+                    Application.Exit();
+                }
+            }
+
+            Control[] ArrControls = groupBox1.Controls.Find("saveProfile" + Properties.Settings.Default.profilenum, true);
+            Debug.Print("saveProfile" + Properties.Settings.Default.profilenum);
+            if (ArrControls.Length == 0) {
+                MessageBox.Show("Did not find any checkboxes", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+            
+            RadioButton rb = (RadioButton)ArrControls[0];
+            rb.Checked = true;
+            FormatTexts();
+            for (int i = 0; i < 4; i++) {
+                Debug.WriteLine(Properties.Settings.Default["save" + ++i]);
+            }
+
+            
+        }
+
+        private void radioButtons_CheckedChanged(object sender, EventArgs e) {
+            RadioButton radioButton = sender as RadioButton;
+            if (!radioButton.Checked) return;
+            int buttonid = (int) int.Parse(radioButton.Name.Replace("saveProfile", ""));
+            TextBox tb = (TextBox) groupBox1.Controls["TextBox" + buttonid];
+            Properties.Settings.Default.profilenum = buttonid;
+            SwapToID(buttonid);
+        }
+
+        private void textBox_Changed(object sender, EventArgs e) {
+            FormatTexts();
+        }
+
+        private void FormatTexts() {
+            TextBox[] texts = groupBox1.Controls.OfType<TextBox>().ToArray();
+            for (int i = 0; i < 4; i++) {
+                texts[i].BackColor = (texts[i].Text == "") ? Color.White : SystemColors.Control;
+                var settings = Properties.Settings.Default["save" + (++i)];
+                if(settings != null) settings = texts[i].Text;
+
+            }
+
+            
+            //todo: save
+        }
+ 
+        private void SwapToID(int id) {
+            if (!Directory.Exists(SAVEPATH)) {
+                Directory.CreateDirectory(SAVEPATH);
+            }else if (!HITMANPATH.Exists) {
+                MessageBox.Show("No files found!!!%$@#%");
+            }
+
+            MoveExistingToStorage();
+            Debug.WriteLine("Done, Copying save now");
+            copySavedFromStorage(id); 
+            Debug.WriteLine("moved?");
+        }
+
+        private void MoveExistingToStorage() {
+            //get ID of source via swapper.txt
+
+            try {   // Open the text file using a stream reader.
+                var swapFile = Path.Combine(HITMANPATH.FullName, "swapper.txt");
+                if (!File.Exists(swapFile)) {
+                    return;
+                }
+                using (StreamReader sr = new StreamReader(swapFile)) {
+                    String line = sr.ReadToEnd();
+                    int fromID;
+                    if (!int.TryParse(line, out fromID)) {
+                        MessageBox.Show("Unknown location to store hitman save - Not A number. Explode world?");
+                        return;
+                    }
+                    Debug.WriteLine("Line: " + line + "| ID:" + fromID);
+                    string saveLocation = Path.Combine(SAVEPATH, "save" + fromID);
+                    if (!Directory.Exists(saveLocation)) {
+                        Directory.CreateDirectory(saveLocation);
+                    }
+                    //TODO: Wipe storage folders & files (autosaves stack up)
+
+                    Debug.WriteLine(HITMANPATH.Parent + HITMANPATH.ToString());
+                    List<DirectoryInfo> subFolders = HITMANPATH.GetDirectories().ToList();
+                    Debug.WriteLine("[" + fromID + "] Moving " + subFolders.Count + " files to %PROFILEPATH%/save" + fromID);
+                    foreach (DirectoryInfo dir in subFolders) {
+                        //dir.MoveTo(Path.Combine(profilename,dir.Name));
+                        List<FileInfo> hitmanFiles = dir.GetFiles("*", SearchOption.TopDirectoryOnly).ToList();
+                        foreach (FileInfo file in hitmanFiles) {
+                            try {
+                                string destDir = Path.Combine(saveLocation, dir.Name);
+                                string destFile = Path.Combine(destDir, file.Name);
+                                if (!new DirectoryInfo(destDir).Exists) {
+                                    Directory.CreateDirectory(destDir);
+                                }
+                                if (File.Exists(destFile)) {
+                                    File.Delete(destFile);
+                                }
+
+                                file.MoveTo(destFile);
+                            } catch (Exception ex) when (ex is UnauthorizedAccessException || ex is IOException) {
+                                Debug.WriteLine("Failed move: " + ex.Message);
+                            }
+                        }
+
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine("The file could not be read: " + e.Message);
+                throw;
+            }
+
+            
+        }
+
+        private void copySavedFromStorage(int id) {
+            string sourceLocation = Path.Combine(SAVEPATH, "save" + id);
+            string saveLocation = HITMANPATH.FullName;
+            if (!Directory.Exists(saveLocation) || !Directory.Exists(sourceLocation)) {
+                //Directory.CreateDirectory(saveLocation);
+                return; //Don't do anything; no need
+            }
+            Debug.WriteLine(HITMANPATH.ToString());
+            List<DirectoryInfo> subFolders = new DirectoryInfo(sourceLocation).GetDirectories().ToList();
+            Debug.WriteLine("[" + id + "] Moving " + subFolders.Count + " files to HITMAN" + id);
+            foreach (DirectoryInfo dir in subFolders) {
+                //dir.MoveTo(Path.Combine(profilename,dir.Name));
+                List<FileInfo> hitmanFiles = dir.GetFiles("*", SearchOption.TopDirectoryOnly).ToList();
+                foreach (FileInfo file in hitmanFiles) {
+                    try {
+                        string destDir = Path.Combine(HITMANPATH.FullName, dir.Name);
+                        string destFile = Path.Combine(destDir, file.Name);
+                        if (!new DirectoryInfo(destDir).Exists) {
+                            Directory.CreateDirectory(destDir);
+                        }
+                        if (File.Exists(destFile)) {
+                            File.Delete(destFile);
+                        }
+                        file.MoveTo(destFile);
+                    } catch (Exception ex) when (ex is UnauthorizedAccessException || ex is IOException) {
+                        Debug.WriteLine("Failed move: " + ex.Message);
+                    }
+                }
+
+            }
+            System.IO.File.WriteAllText(Path.Combine(HITMANPATH.FullName,"swapper.txt"), id.ToString());
+        }
+    }
+}
